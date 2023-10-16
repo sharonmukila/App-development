18bf435a27bd155d8fa1c376552569aa7848fa35
Mukila S <110587466+sharonmukila@users.noreply.github.com>
2023-10-16 13:37:49 +0530

    Add files via upload

---
+++ Day 23/Frontend/src/ComponentS/Terms.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import NavbarLand from './NavbarLand'
+import Footer from './Footer'
+
+export default function Terms() {
+  return (
+        <div className='ttt'>
+                <NavbarLand/>
+      
+   < div className='termsAndCondition'>
+
+
+     <div className='terms'>
+        Terms And Condition For Crime Hut....
+     </div>
+
+     <div className='date'>
+     Last Updated: [25/09/2023]
+        </div>
+        <div className='1'>
+        <b>Acceptance of Terms and Conditions:</b>
+
+By accessing or using the Crime Reporting Application (the "App"), you agree to comply with and be bound by these Terms and Conditions ("T&C"). If you do not agree to these T&C, please do not use the App.
+        </div>
+        <div className='2'>
+       <b> Description of the App:</b>
+
+The Crime Reporting Application is a platform that allows users to report and share information about criminal activities and safety-related incidents in their area.
+        </div>
+        <div className='3'>
+        User Registration
+
+ To use certain features of the App, you may be required to register and create an account. You agree to provide accurate and complete information during the registration process.
+You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account. You must immediately notify us of any unauthorized use of your account.
+        </div>
+        <div className='4'>
+        <b>User Content:</b>
+Users may submit text, images, videos, and other content ("User Content") to the App. By submitting User Content, you grant the App owner a non-exclusive, worldwide, royalty-free, and fully sublicensable right to use, reproduce, modify, adapt, publish, translate, create derivative works from, distribute, and display such User Content.
+You represent and warrant that you have all necessary rights to submit User Content and that the content does not infringe upon the rights of any third party.
+        </div>
+
+        <div className="5">
+        <b>User Content:</b>
+Users may submit text, images, videos, and other content ("User Content") to the App. By submitting User Content, you grant the App owner a non-exclusive, worldwide, royalty-free, and fully sublicensable right to use, reproduce, modify, adapt, publish, translate, create derivative works from, distribute, and display such User Content.
+ You represent and warrant that you have all necessary rights to submit User Content and that the content does not infringe upon the rights of any third party.
+        </div>
+
+        <div className='6'>
+
+       <b>  Prohibited Activities:   </b>
+ You agree not to use the App for any unlawful or harmful purposes, including but not limited to.
+Submitting false or misleading information.
+Harassing, threatening, or abusing other users.
+Posting content that promotes violence, discrimination, or hate speech.
+Violating any applicable laws or regulations.
+The App owner reserves the right to remove or block access to any User Content or user accounts that violate these T&C.
+        </div>
+
+        <div className="7">
+      <b>  Privacy:</b>
+ Your use of the App is subject to our Privacy Policy, which can be found at [Link to Privacy Policy]. By using the App, you consent to the collection, use, and sharing of your information as described in the Privacy Policy.
+        </div>
+
+        <div className="8">
+      <b>  Disclaimer of Warranties:</b>
+The App owner makes no representations or warranties about the accuracy, reliability, completeness, or suitability of the information provided on the App. You use the App and its information at your own risk.
+        </div>
+        <div className='9'>
+       <b>
+         Limitation of Liability:
+        </b> 
+ To the maximum extent permitted by law, the App owner shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses, resulting from (i) your use or inability to use the App, (ii) any unauthorized access to or use of our servers and/or any personal information stored therein, (iii) any errors or omissions in any content or User Content, or (iv) any other matter relating to the App
+        </div>
+
+        <div className='10'>
+       <b> Modifications to the T&C:</b>
+The App owner reserves the right to modify these T&C at any time. We will notify users of any material changes by posting the updated T&C on the App or through other means. Your continued use of the App after such modifications constitutes your acceptance of the updated T&C.
+        </div>
+
+        <div className="11">
+       <b> Governing Law:</b>
+ These T&C are governed by and construed in accordance with the laws of [Jurisdiction]. Any dispute arising out of or related to these T&C shall be subject to the exclusive jurisdiction of the state and federal courts located in [Jurisdiction].
+        </div>
+
+        <div className="12">
+      <b>  Contact Information:</b>
+For questions or concerns about these T&C, please contact us at [CrimeHut@gmail.com].
+By using the Crime Reporting Application, you agree to abide by these Terms and Conditions. If you do not agree with any of these terms, please refrain from using the App.
+[Crime Hut/Captivators]
+
+
+   </div>
+</div>
+<Footer/>
+</div>
+  )
+}
---
+++ Day 23/Frontend/src/component/Chat.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import PrimarySearchAppBar from './Navbar';
+import './Chat.css'; // Import your CSS file for styling
+import Navbar from './Navbar';
+
+function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+  const [initialAdminMessageSent, setInitialAdminMessageSent] = useState(false);
+
+  useEffect(() => {
+    // Simulate receiving random messages (replace with actual data)
+    const interval = setInterval(() => {
+      if (initialAdminMessageSent) {
+        const allMessages = [
+          { sender: 'user', text: 'User message 1' },
+          { sender: 'admin', text: 'Admin message 1' },
+          { sender: 'user', text: 'User message 2' },
+          { sender: 'admin', text: 'Admin message 2' }
+          // Add more messages here as needed
+        ];
+
+        const randomIndex = Math.floor(Math.random() * allMessages.length);
+        const randomMessage = allMessages[randomIndex];
+
+        setMessages([...messages, randomMessage]);
+      }
+    }, 3000); // Simulate receiving a new message every 3 seconds
+
+    // Cleanup the interval on unmount
+    return () => clearInterval(interval);
+  }, [messages, initialAdminMessageSent]);
+
+  const handleInputChange = (e) => {
+    setNewMessage(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (!initialAdminMessageSent) {
+      // Send the initial admin message
+      const adminMessage = { sender: 'admin', text: 'Hi there! How can I assist you today?' };
+      setMessages([...messages, adminMessage]);
+      setInitialAdminMessageSent(true);
+    } else if (newMessage.trim() !== '') {
+      const userMessage = { sender: 'user', text: newMessage };
+      setMessages([...messages, userMessage]);
+      setNewMessage('');
+
+      // Simulate an admin response after a delay (e.g., 2 seconds)
+      setTimeout(() => {
+        const adminMessage = { sender: 'admin', text: 'Thank you for your message! We will get back to you shortly.' };
+        setMessages([...messages, adminMessage]);
+      }, 2000); // Simulate a 2-second delay before the admin responds
+    }
+  };
+
+  return (
+    <div className='chat'>
+      <Navbar/>
+     
+      <div className="chat-container">
+        <h1>User-Admin Chat</h1>
+        <div className="chat-messages">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`message ${message.sender === 'user' ? 'user' : 'admin'}`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="message-input">
+          <input
+            type="text"
+            placeholder="Type your message..."
+            value={newMessage}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default Chat;
